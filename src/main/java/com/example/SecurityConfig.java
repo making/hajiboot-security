72f71de671a01d4c73cd9366944938e67dccea80
Toshiaki Maki <makingx@gmail.com>
2024-10-08 13:39:59 +0900

    Update filterChain

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -1,24 +1,25 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Component
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests() //
-            .mvcMatchers("/webjars/**", "/css/**").permitAll() //
-            .anyRequest().authenticated() //
-            .and() //
-            .formLogin() //
-            .loginProcessingUrl("/login") //
-            .loginPage("/loginForm").permitAll() //
-            .failureUrl("/loginForm?error") //
-            .defaultSuccessUrl("/customers", true) //
-            .and() //
-            .logout().logoutSuccessUrl("/loginForm");
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(requests -> requests //
+						.mvcMatchers("/webjars/**").permitAll() //
+						.mvcMatchers("/css/**").permitAll() //
+						.anyRequest().authenticated()) //
+				.formLogin(login -> login //
+						.loginProcessingUrl("/login") //
+						.loginPage("/loginForm").permitAll() //
+						.failureUrl("/loginForm?error") //
+						.defaultSuccessUrl("/customers", true)) //
+				.logout(logout -> logout.logoutSuccessUrl("/loginForm"))
+				.build();
+	}
 }
